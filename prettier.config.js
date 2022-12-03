2836517bf1ee8a8a0bee400b5b0d514c91808eb9
nikkii94 <k.nikolett17@gmail.com>
2022-12-03 14:17:55 +0100

    FireShip AI Photo Generator App

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+    "semi": true,
+    "trailingComma": "all",
+    "avoid": "avoid",
+    "singleQuote": true,
+    "printWidth": 80,
+    "tabWidth": 4,
+    "useTabs": false,
+    "endOfLine": "auto"
+};
